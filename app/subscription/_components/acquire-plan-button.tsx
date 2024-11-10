72fed48ce9989f9b18e1402c5803695e2d475df5
GaelGesser <gabrielhenrig@outlook.com>
2024-11-10 17:49:57 -0300

    feat: add stripe webhook that handles subscriptions

--- app/subscription/_components/acquire-plan-button.tsx
+++ app/subscription/_components/acquire-plan-button.tsx
@@ -3,7 +3,11 @@
 import { Button } from "@/app/_components/ui/button";
 import { createStripeCheckout } from "../_actions/create-stripe-checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
+
 const AcquirePlanButton = () => {
+  const { user } = useUser();
   const handleAcquirePlanClick = async () => {
     const { sessionId } = await createStripeCheckout();
 
@@ -22,14 +26,28 @@ const AcquirePlanButton = () => {
     await stripe.redirectToCheckout({ sessionId });
   };
 
-  return (
-    <Button
-      onClick={handleAcquirePlanClick}
-      className="w-full rounded-full font-bold"
-    >
-      Adquirir plano
-    </Button>
-  );
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan === "premium";
+
+  if (hasPremiumPlan) {
+    return (
+      <Button asChild className="w-full rounded-full font-bold" variant="link">
+        <Link
+          href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL as string}?prefilled_email=${user.emailAddresses[0].emailAddress}`}
+        >
+          Gerenciar plano
+        </Link>
+      </Button>
+    );
+  } else {
+    return (
+      <Button
+        onClick={handleAcquirePlanClick}
+        className="w-full rounded-full font-bold"
+      >
+        Adquirir plano
+      </Button>
+    );
+  }
 };
 
 export default AcquirePlanButton;
